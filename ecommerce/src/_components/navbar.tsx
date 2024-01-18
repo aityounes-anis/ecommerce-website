6ff8fc222a55427510d49f03c19f6317b631e283
aityounes-anis <aa.aityounes.anis@gmail.com>
2024-01-18 16:04:59 +0100

    Update and remove a cart product

--- ecommerce/src/_components/navbar.tsx
+++ ecommerce/src/_components/navbar.tsx
@@ -1,7 +1,10 @@
-import { AskSignInOut, Cart, Logo } from ".";
+"use client";
+
+import { AskSignInOut, Cart, CartItem, Logo } from ".";
+
+import { useState } from "react";
 
 import { UserRound } from "lucide-react";
-
 import {
   Dialog,
   DialogContent,
@@ -11,7 +14,39 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
+import { ProductsType } from "./Cart";
+
 const Navbar = () => {
+  const [products, setProducts] = useState<ProductsType[]>([
+    {
+      id: 1,
+      name: "Product 1",
+      quantity: 1,
+      price: 12,
+      image: "/../../images/products/a1.png",
+    },
+  ]);
+
+  const productsQuantity = products.length;
+
+  const updateCartProducts = (productId: number, newQuantity: number) => {
+    const updatedCartProducts = products.map((product) => {
+      if (product.id === productId) {
+        return { ...product, quantity: newQuantity };
+      }
+      return product;
+    });
+
+    setProducts(updatedCartProducts);
+  };
+
+  const removeProduct = (productId: number) => {
+    const newCartProducts = products.filter(
+      (product) => product.id !== productId
+    );
+    setProducts(newCartProducts);
+  };
+
   return (
     <nav className="w-full py-3 sticky top-0 left-0 bg-slate-100/95 z-50 shadow-sm shadow-black">
       <div className="max-w-screen-2xl flex items-center justify-between m-auto py-4 px-4 md:max-w-[800px] lg:max-w-[1275px]">
@@ -34,7 +69,12 @@ const Navbar = () => {
             </DialogContent>
           </Dialog>
 
-          <Cart />
+          <Cart
+            products={products}
+            productsQuantity={productsQuantity}
+            updateCartProducts={updateCartProducts}
+            removeProduct={removeProduct}
+          />
         </div>
       </div>
     </nav>
