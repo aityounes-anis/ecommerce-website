f2a0467726e8713d38b71292aad8014ef90221c5
aityounes-anis <aa.aityounes.anis@gmail.com>
2024-01-24 13:07:50 +0100

    All The products pf the store

---
+++ ecommerce/src/data/products.ts
@@ -0,0 +1,25 @@
+import { ProductsType } from "@/_components/Cart";
+
+export const data: ProductsType[] = [
+  {
+    id: 0,
+    name: "Product 1",
+    quantity: 1,
+    price: 12,
+    image: "/../../images/products/a1.png",
+  },
+  {
+    id: 1,
+    name: "Product 2",
+    quantity: 1,
+    price: 12,
+    image: "/../../images/products/a2.png",
+  },
+  {
+    id: 2,
+    name: "Product 3",
+    quantity: 1,
+    price: 12,
+    image: "/../../images/products/a3.png",
+  },
+];
